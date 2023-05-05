c1787c52fe96e2ef3c9079ef9443f83bb543e563
null <null>
2023-05-05 10:26:47 -0500

    pin assignment

--- mem_contents.sv
+++ mem_contents.sv
@@ -1,4 +1,13 @@
 ////memory contents: to be filled in later. hard code to test data for cp1. 
-//module memory_contents(input addr[10:0])
-//    
-//endmodule
+module memory_contents( input logic [14:0]	addr,
+				  output logic [7:0]	data
+					 );
+
+	parameter ADDR_WIDTH = 15;
+   parameter DATA_WIDTH =  8;
+   logic [ADDR_WIDTH-1:0] addr_reg;
+
+	parameter [0:2**ADDR_WIDTH-1][DATA_WIDTH-1:0] ROM = {32767{8'hEE}}; 
+	assign data = ROM[addr];
+
+endmodule  
---
+++ output_files/ece385final.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 22.1std.1 Build 917 02/14/2023 SC Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484ES) Path("/home/user/Documents/ece385/ece385_finalproject/output_files/") File("ece385final.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
